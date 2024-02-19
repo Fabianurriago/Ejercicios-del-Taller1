e060645a942dcd6d020553958b8b320382acbc16
unknown <u20232218051@usco.edu.co>
2024-02-19 15:54:39 -0500

    Mi primer comentario

---
+++ src/main/java/com/mycompany/ejercicioscondicionales/EjerciciosCondicionales.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicioscondicionales;
+
+/**
+ *
+ * @autor Fabian Arley Urriago Montealegre 
+ */
+public class EjerciciosCondicionales {
+
+    public static void main(String[] args) {
+        System.out.println("escirbir la cantidad de ");
+        System.out.println("hola mundo ");
+       
+        
+                
+    }
+}
